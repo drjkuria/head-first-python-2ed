7d1234b1bfb0ca7b15282f6b6404c2d9494d1fac
Jonah Kuria <jonah.kuria@hotmail.com>
2018-11-07 10:52:43 +0200

    [ch-05] add search_for route

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from vsearch import search_for_letters
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello() -> str:
     return 'Hello world from Flask!'
+
+@app.route('/search_for')
+def do_search() -> str:
+    return str(search_for_letters('life, the universe, and everything', 'eiru!'))
 
 app.run()
