1c58c909539a40fd4f416e0efd4b5cc35368324c
Jonah Kuria <jonah.kuria@hotmail.com>
2018-11-09 12:04:10 +0200

    [ch_05] add debug mode to run method

--- webapp/vsearch_for_web.py
+++ webapp/vsearch_for_web.py
@@ -16,4 +16,4 @@ def entry_page() -> 'html':
     return render_template('entry.html', 
                             the_title='Welcome to search_for_letters on the web!')
 
-app.run()
+app.run(debug=True)
