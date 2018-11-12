0766f768a05559411aba472f3228f2004ce9f6c7
Jonah Kuria <jonah.kuria@hotmail.com>
2018-11-12 09:11:29 +0200

    [ch_05] delete hello function and use multiple urls for entry page

--- webapp/vsearch_for_web.py
+++ webapp/vsearch_for_web.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 from vsearch import search_for_letters
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello() -> '302':
-    return redirect('/entry')
 
 @app.route('/search_for', methods=['POST'])
 def do_search() -> 'html':
@@ -19,6 +15,7 @@ def do_search() -> 'html':
                             the_title=title,
                             the_results=results)
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', 
