9fee6ae36d1bfe80a2218da7dd3552ebf21421f8
Jonah Kuria <jonah.kuria@hotmail.com>
2018-11-13 14:04:53 +0200

    [ch_06] - add log request function

--- webapp/vsearch_for_web.py
+++ webapp/vsearch_for_web.py
@@ -3,12 +3,17 @@ from vsearch import search_for_letters
 
 app = Flask(__name__)
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
+
 @app.route('/search_for', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search_for_letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                             the_phrase=phrase,
                             the_letters=letters,
