8d0f445274c4e0718cd6c7e5fe1623e11a0da875
Jonah Kuria <jonah.kuria@hotmail.com>
2018-11-16 10:54:22 +0200

    [ch_06] - update log request

--- webapp/vsearch_for_web.py
+++ webapp/vsearch_for_web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 @app.route('/search_for', methods=['POST'])
 def do_search() -> 'html':
