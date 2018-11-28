e03747d8ce02cb28d1776f8d0ad388e4fdc23b67
Jonah Kuria <jonah.kuria@hotmail.com>
2018-11-28 11:38:08 +0200

    [ch_09] - refactor view the log

--- webapp/vsearch_for_web.py
+++ webapp/vsearch_for_web.py
@@ -4,56 +4,54 @@ from DBcm import UseDatabase
 
 app = Flask(__name__)
 
-app.config['dbconfig'] = {
-    'host': '127.0.0.1',
-    'user': 'vsearch',
-    'password': 'vsearchpasswd',
-    'database': 'vsearchlogDB'
-}
+app.config['dbconfig'] = {'host': '127.0.0.1',
+                          'user': 'vsearch',
+                          'password': 'vsearchpasswd',
+                          'database': 'vsearchlogDB', }
 
 def log_request(req: 'flask_request', res: str) -> None:
     """Log details of the web request and the results."""
-
     with UseDatabase(app.config['dbconfig']) as cursor:
         _SQL = """insert into log
                   (phrase, letters, ip, browser_string, results)
                   values
-                  (%s, %s, %s, %s, %s)"""
-
-    cursor.execute(_SQL, (req.form['phrase'],
-                          req.form['letters'],
-                          req.remote_addr,
-                          req.user_agent.browser,
-                          res,))
+                  (%s, %s, %s, %s, %s)
+        """
+        cursor.execute(_SQL, (req.form['phrase'],
+                            req.form['letters'],
+                            req.remote_addr,
+                            req.user_agent.browser,
+                            res, ))
 
 @app.route('/search_for', methods=['POST'])
 def do_search() -> 'html':
+    """Extract the posted data; perform search; return results."""
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search_for_letters(phrase, letters))
     log_request(request, results)
     return render_template('results.html',
+                            the_title=title,
                             the_phrase=phrase,
                             the_letters=letters,
-                            the_title=title,
                             the_results=results)
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
+    """Display this webapp's HTML form."""
     return render_template('entry.html',
                             the_title='Welcome to search_for_letters on the web!')
 
 @app.route('/viewlog')
 def view_the_log() -> 'html':
-    contents = []
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            for item in line.split('|'):
-                contents[-1].append(escape(item))
-    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    """Display the contents of the log file as a HTML table."""
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
     return render_template('viewlog.html',
                             the_title='View Log',
                             the_row_titles=titles,
