0c45c379a4104e92ca687aaad6821d4327509a8e
Jonah Kuria <jonah.kuria@hotmail.com>
2018-11-28 09:36:11 +0200

    [ch_09] - add enter method

--- webapp/DBcm.py
+++ webapp/DBcm.py
@@ -4,3 +4,8 @@ class UseDatabase:
 
     def __init__(self, config: dict) -> None:
         self.configuration = config
+
+    def __enter__(self) -> 'cursor':
+        self.conn = mysql.connector.connect(**self.configuration)
+        self.cursor = self.conn.cursor()
+        return cursor
