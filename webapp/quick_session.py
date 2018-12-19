788ad04f82e11e80756c33a86be468982e7acb8d
Jonah Kuria <jonah.kuria@hotmail.com>
2018-12-19 15:36:12 +0200

    [ch_10] - add quick session demo

---
+++ webapp/quick_session.py
@@ -0,0 +1,17 @@
+from flask import Flask, session
+
+app = Flask(__name__)
+
+app.secret_key = 'YouWillNeverGuess'
+
+@app.route('/setuser/<user>')
+def setuser(user: str) -> str:
+    session['user'] = user
+    return 'User value set to: ' + session['user']
+
+@app.route('/getuser')
+def getuser() -> str:
+    return 'User value is currently set to: ' + session['user']
+
+if __name__ == '__main__':
+    app.run(debug=True)
