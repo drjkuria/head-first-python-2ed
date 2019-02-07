c569aebd823deebcb92b088fa71e7d821f0b1c6f
Jonah Kuria <jonah.kuria@hotmail.com>
2019-02-07 15:52:01 +0200

    [ch_11] - raise custom connection error in use database enter

--- webapp/DBcm.py
+++ webapp/DBcm.py
@@ -1,14 +1,20 @@
 import mysql.connector
 
+class ConnectionError(Exception):
+    pass
+
 class UseDatabase:
 
     def __init__(self, config: dict) -> None:
         self.configuration = config
 
     def __enter__(self) -> 'cursor':
-        self.conn = mysql.connector.connect(**self.configuration)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        try:
+            self.conn = mysql.connector.connect(**self.configuration)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except mysql.connector.errors.InterfaceError as err:
+            raise ConnectionError(err)
 
     def __exit__(self, exc_type, exc_value, exc_trace) -> None:
         self.conn.commit()
