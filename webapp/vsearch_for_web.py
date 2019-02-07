4b7cd1e63fb986b740d264e929d7838a78e566c5
Jonah Kuria <jonah.kuria@hotmail.com>
2019-02-07 14:27:54 +0200

    [ch_11] - handle database errors in do search

--- webapp/vsearch_for_web.py
+++ webapp/vsearch_for_web.py
@@ -41,7 +41,10 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search_for_letters(phrase, letters))
-    log_request(request, results)
+    try:
+        log_request(request, results)
+    except Exception as err:
+        print('***** Logging failed with this error:', str(err))
     return render_template('results.html',
                             the_title=title,
                             the_phrase=phrase,
